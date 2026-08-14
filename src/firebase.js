cf5e60a8833825039f72685c8cb21b02f35f9b75
cinthyatamarez-dotcom <cinthyatamarez@gmail.com>
2026-08-14 13:04:14 -0400

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbcU0FuMMa3EnFhLQYIGLnCVKGbXK9Twk",
@@ -20,3 +21,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
